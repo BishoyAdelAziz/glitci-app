529ec0b96249a5ba087091dda2c4c834241a6095
Bishoy Adel Aziz <bishoy.adel5555@gmail.com>
2026-01-29 06:26:44 +0200

    fixed layout error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "@/lib/queryClient";
+import { QueryClientProvider } from "@/lib/queryClient";
 import { ThemeProvider } from "@/providers/themeProvider";
 import "./globals.css";
 import { TokenRefreshProvider } from "@/providers/TokenRefreshProvider";
@@ -34,7 +33,7 @@ export default function RootLayout({
       </head>
 
       <body suppressHydrationWarning>
-        <QueryClientProvider client={queryClient}>
+        <QueryClientProvider>
           <TokenRefreshProvider>
             <ThemeProvider>{children}</ThemeProvider>
           </TokenRefreshProvider>
