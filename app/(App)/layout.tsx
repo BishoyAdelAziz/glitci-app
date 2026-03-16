5ee2943c94e9d04152478caf0926c3078b7a5b18
Bishoy Adel Aziz <bishoy.adel5555@gmail.com>
2026-03-16 18:48:26 +0200

    fix; fied layout

--- app/(App)/layout.tsx
+++ app/(App)/layout.tsx
@@ -1,22 +1,23 @@
+import { Suspense } from "react";
 import NavHeader from "@/components/layout/nav";
 import ControllersNav from "@/components/layout/controllers";
 import { Metadata } from "next";
+
 export const metadata: Metadata = {
   title: `${process.env.NEXT_PUBLIC_APP_NAME} App`,
   description: "Here Your Can Manage Your Application",
 };
+
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <section className="container mx-auto">
-      <NavHeader />
+      <Suspense fallback={null}>
+        <NavHeader />
+      </Suspense>
 
-      {/* App shell */}
-      <div className="relative  flex">
-        {/* Controllers pinned inside app width */}
+      <div className="relative flex">
         <ControllersNav />
-
-        {/* Main page content */}
-        <main className="flex-1  pt-20 ">
+        <main className="flex-1 pt-20">
           <section className="bg-white dark:bg-gray-900 p-5 md:p-10 lg:p-20 rounded-4xl">
             {children}
           </section>
