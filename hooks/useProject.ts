83279ff1dce1edafe66fcd9fee803d823854f975
Bishoy Adel Aziz <bishoy.adel5555@gmail.com>
2026-04-16 15:48:15 +0200

    fix: fixed project type

--- hooks/useProject.ts
+++ hooks/useProject.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProjectById } from "@/services/api/projects";
 import { ParamValue } from "next/dist/server/request/params";
-export function useProject(id?: string | ParamValue) {
+export function useProject(id?: string | null) {
   const { data, isLoading, isError, error, refetch } = useQuery({
     queryKey: ["projects", id],
     queryFn: () => getProjectById(id as string),
